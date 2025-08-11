805f75d51454d6dbfb7b593cf0b9d77f74679985
Juawo <joaopedrojp2703@gmail.com>
2025-08-10 22:40:35 -0300

    Add 'Category' property to Card model and update migration files

--- QuestAnswerApi/Migrations/20250811013812_CategoryCollumInCard.Designer.cs
+++ QuestAnswerApi/Migrations/20250811013812_CategoryCollumInCard.Designer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using QuestAnswerApi.Repositories;
@@ -11,9 +12,11 @@ using QuestAnswerApi.Repositories;
 namespace QuestAnswerApi.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250811013812_CategoryCollumInCard")]
+    partial class CategoryCollumInCard
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
