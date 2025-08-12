f7d0e806715859ae339311311a1940d4aeca4b15
Juawo <joaopedrojp2703@gmail.com>
2025-08-12 11:55:48 -0300

    Add CardServices class to manage card operations

---
+++ QuestAnswerApi/Service/CardServices/CardServices.cs
@@ -0,0 +1,37 @@
+using QuestAnswerApi.Models;
+using QuestAnswerApi.Repositories.CardRepositories;
+
+namespace QuestAnswerApi.Service.CardServices;
+
+public class CardServices : ICardServices
+{
+    private readonly CardRepository _cardRepository;
+    public CardServices(CardRepository cardRepository)
+    {
+        this._cardRepository = cardRepository;
+    }
+    public async Task CreateCard(Card card)
+    {
+        await _cardRepository.CreateCardAsync(card);
+    }
+
+    public async Task<bool> DeleteCard(long id)
+    {
+        return await _cardRepository.DeleteCardAsync(id);
+    }
+
+    public async Task<IEnumerable<Card>> GetAllCards()
+    {
+        return await _cardRepository.GetAllCardsAsync();
+    }
+
+    public async Task<Card?> GetCardById(long id)
+    {
+        return await _cardRepository.GetCardByIdAsync(id);
+    }
+
+    public async Task<bool> UpdateCard(Card card)
+    {
+        return await _cardRepository.UpdateCardAsync(card);
+    }
+}
