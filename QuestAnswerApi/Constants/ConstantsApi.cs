036d6c2df208ea189d159250fe6b4c1cb9c64ba6
Juawo <joaopedrojp2703@gmail.com>
2026-05-13 22:46:55 -0300

    Add ConstantsApi class for API key authorization

---
+++ QuestAnswerApi/Constants/ConstantsApi.cs
@@ -0,0 +1,7 @@
+namespace QuestAnswerApi.Constants;
+
+public static class ConstantsApi
+{
+    public const string ApiKeyHeaderName = "X-Api-Key";
+    public const string ApiKeyName = "ApiKey";
+}
