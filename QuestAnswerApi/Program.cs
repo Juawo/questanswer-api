c63c364f76e8a793c5ba25d820f3557e9bf6c7b9
Juawo <joaopedrojp2703@gmail.com>
2026-05-13 22:47:24 -0300

    Add ApiKeyMiddleware for API key validation and update Program.cs for middleware integration

--- QuestAnswerApi/Program.cs
+++ QuestAnswerApi/Program.cs
@@ -1,28 +1,35 @@
 using Microsoft.EntityFrameworkCore;
+using QuestAnswerApi.Interfaces;
+using QuestAnswerApi.Middlewares;
 using QuestAnswerApi.Repositories;
 using QuestAnswerApi.Repositories.CardRepositories;
 using QuestAnswerApi.Service.CardServices;
+using QuestAnswerApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
+//
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowAll", policy =>
+//     {
+//         policy.AllowAnyOrigin()
+//             .AllowAnyMethod()
+//             .AllowAnyHeader();
+//     });
+// });
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddTransient<IApiKeyValidation, ApiKeyValidation>();
 builder.Services.AddScoped<CardRepository>();
 builder.Services.AddScoped<ICardServices, CardServices>();
+
 var app = builder.Build();
-app.UseCors("AllowAll");
+app.UseMiddleware<ApiKeyMiddleware>();
+// app.UseCors("AllowAll");
+
 app.MapControllers();
 app.Run();
